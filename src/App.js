e33758687cd554b30ef94b409b34238c380a2b9c
Amit Nautiyal <amit.nautiyal@crssoftware.co.nz>
2018-09-12 09:15:46 +1200

    remove console.log

--- src/App.js
+++ src/App.js
@@ -62,9 +62,6 @@ class App extends Component {
 
   // This function sets the selected list item to the input value
   listItemClcikHandler(e) {
-    let ele = document.querySelectorAll('.filter-input')[0]
-    console.log(ele)
-    ele.focus()
     this.setState({
       input: e.target.innerHTML,
       showList: false
